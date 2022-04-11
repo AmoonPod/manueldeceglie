e68570d59e42c2801aa05ce3ac691debc1a576df
AmoonPod <80224270+AmoonPod@users.noreply.github.com>
2022-04-11 11:26:55 +0200

    Cooming soon... page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,32 @@
-import '../styles/globals.css'
+import { motion } from "framer-motion";
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps, router }) {
+  return (
+    <motion.div
+      key={router.route}
+      initial="pageInitial"
+      animate="pageAnimate"
+      variants={{
+        pageInitial: {
+          opacity: 0,
+          transition: {
+            duration: 2,
+            ease: "easeInOut",
+          },
+        },
+        pageAnimate: {
+          opacity: 1,
+          transition: {
+            duration: 1.5,
+            ease: "easeInOut",
+          },
+        },
+      }}
+    >
+      <Component {...pageProps} />
+    </motion.div>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      mio: ["Comfortaa"],
+      mio1: ["Montserrat"],
+      share_tech_mono: ["Share Tech Mono", "monospace"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
